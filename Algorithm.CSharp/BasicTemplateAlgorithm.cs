b29aafaca6e779b9019b5e476663ee24fd1821ee
jich <19599009+jich2021@users.noreply.github.com>
2021-08-13 20:34:40 +0200

    Backtesting Europe

--- Algorithm.CSharp/BasicTemplateAlgorithm.cs
+++ Algorithm.CSharp/BasicTemplateAlgorithm.cs
@@ -28,22 +28,24 @@ namespace QuantConnect.Algorithm.CSharp
     /// <meta name="tag" content="trading and orders" />
     public class BasicTemplateAlgorithm : QCAlgorithm, IRegressionAlgorithmDefinition
     {
-        private Symbol _spy = QuantConnect.Symbol.Create("SPY", SecurityType.Equity, Market.USA);
+        //private Symbol _spy = QuantConnect.Symbol.Create("SPY", SecurityType.Equity, Market.USA);
 
         /// <summary>
         /// Initialise the data and resolution required, as well as the cash and start-end dates for your algorithm. All algorithms must initialized.
         /// </summary>
         public override void Initialize()
         {
-            SetStartDate(2013, 10, 07);  //Set Start Date
-            SetEndDate(2013, 10, 11);    //Set End Date
-            SetCash(100000);             //Set Strategy Cash
+            SetStartDate(2021, 01, 04);  //Set Start Date
+            SetEndDate(2021, 03, 31);    //Set End Date
+            //SetCash("EUR", 100000m, 1.219m);
+            SetAccountCurrency("EUR");
+            SetCash(100000); //Set Strategy Cash
 
             // Find more symbols here: http://quantconnect.com/data
             // Forex, CFD, Equities Resolutions: Tick, Second, Minute, Hour, Daily.
             // Futures Resolution: Tick, Second, Minute
             // Options Resolution: Minute Only.
-            AddEquity("SPY", Resolution.Minute);
+            AddEquity("SAP", Resolution.Daily, "EUROPE");
 
             // There are other assets with similar methods. See "Selecting Options" etc for more details.
             // AddFuture, AddForex, AddCfd, AddOption
@@ -57,7 +59,8 @@ namespace QuantConnect.Algorithm.CSharp
         {
             if (!Portfolio.Invested)
             {
-                SetHoldings(_spy, 1);
+                //SetHoldings("SAP", 1);
+                MarketOrder("SAP", 10);
                 Debug("Purchased Stock");
             }
         }
